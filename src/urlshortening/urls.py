c97f6dfd64a0ede73e7adf0befc112339e553c7d
Kai <usochan@gmail.com>
2019-04-01 18:31:41 +0800

    Update urls.py
    remove expand
    remove short
    remove invalidate
    remove expand in /r/

--- src/urlshortening/urls.py
+++ src/urlshortening/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from urlshortening.views import get_full_link, get_short_link, get_redirect, invalidate
 
 urlpatterns = [
-    url(r'^expand/(?P<short_id>.+)/$', get_full_link),
-    url(r'^short/$', get_short_link),
-    url(r'^invalidate/$', invalidate),
-    url(r'^{}/expand/(?P<short_id>.+)/$'.format(settings.REDIRECT_PREFIX), get_redirect)
+#     url(r'^expand/(?P<short_id>.+)/$', get_full_link),
+#     url(r'^short/$', get_short_link),
+#     url(r'^invalidate/$', invalidate),
+    url(r'^{}/(?P<short_id>.+)/$'.format(settings.REDIRECT_PREFIX), get_redirect)
 ]
